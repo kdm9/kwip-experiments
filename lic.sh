ad7c0ae9396f1c55c0c2705362c369b775fffea0
Kevin Murray <spam@kdmurray.id.au>
2015-08-18 19:21:04 +1000

    Remove old crap

--- lic.sh
+++
@@ -1,3 +0,0 @@
-set -xe
-ls data/*.fq.gz |parallel --verbose \
-	load-into-counting.py -x 5e8 -N 1 -k 20 -b -s tsv {.}.ct.gz {}
--- map.sh
+++
@@ -1,7 +0,0 @@
-set -xe
-bwa index data/genome.fa
-bwa mem -p data/genome.fa data/reads.fq.gz | \
-	samtools view -Su - | \
-	samtools sort -o data/reads_s.bam -T ~/tmp
-
-samtools index data/reads_s.bam
