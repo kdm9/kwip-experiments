1aded01a7df95e5312f5e4da71b1f5e5170e35ce
Kevin Murray <spam@kdmurray.id.au>
2016-03-12 18:38:18 +1100

    Rewrite genome simulation

--- coalescent/scripts/splitfa.py
+++ coalescent/scripts/splitfa.py
@@ -3,18 +3,37 @@ from __future__ import print_function, absolute_import, division
 import screed
 import docopt
 
+def seq2fa(name, seq, linelen=80):
+    lines = ['>{}'.format(name), ]
+
+    for start in range(0, len(seq), linelen):
+        lines.append(seq[start:start + linelen])
+
+    return '\n'.join(lines) + '\n'
+
 CLI = """
 USAGE:
-    fasplit <fasta> <prefix>
+    splitfa <fasta> <prefix> [<name> ...]
 """
 
 opts = docopt.docopt(CLI)
 prefix = opts['<prefix>']
+names = opts['<name>']
+
+orig_names = []
+with screed.open(opts['<fasta>']) as fh:
+    for record in fh:
+        orig_names.append(int(record.name))
+
+newnames = {}
+for orig, new in zip(sorted(orig_names), names):
+    newnames[str(orig)] = new
 
 with screed.open(opts['<fasta>']) as fh:
     for record in fh:
-        fname = "{}{}.fasta".format(prefix, record.name)
+        name = newnames[record.name]
+        fname = "{}{}.fasta".format(prefix, name)
         with open(fname, 'w') as ofh:
-            print(">", record.name, sep='', file=ofh)
-            print(str(record.sequence).translate({'-': ''}), file=ofh)
+            seq = str(record.sequence).translate({'-': ''})
+            print(seq2fa(name, seq), file=ofh)
 
